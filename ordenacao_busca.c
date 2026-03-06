786573a908f8dccccad6f0b1478814f62c9b8a13
anna karolina <156126433+nanacodez@users.noreply.github.com>
2026-03-06 18:20:40 -0300

    correcao de codigo

--- ordenacao_busca.c
+++ ordenacao_busca.c
@@ -4,7 +4,7 @@
 
 int le_tamanho_vetor () {
     int t;
-    printf("Digite o tamanho do vetor: ");
+    printf("digite o tamanho do vetor: ");
     scanf ("%d", &t);
     return t;
 }
@@ -17,7 +17,7 @@ void le_vetor (int *v, int tamanho){
 } 
 
 void mostra_vetor (int *v, int tamanho) {
-    printf ("\nveja seu vetor:\n");
+    //printf ("\nveja seu vetor:\n");
     for (int i=0; i<tamanho; i++) {
         printf("%d ", v[i]);
     }
@@ -26,8 +26,29 @@ void mostra_vetor (int *v, int tamanho) {
 
 void preenche_vetor (int *v, int tamanho) {
     for (int i=0; i<tamanho; i++) {
-        v[i] =  rand() % (tamanho * 10);
+        v[i] =  rand() % (tamanho * 10); //preenche com numeros ate 10 vezes o tamanho do vetor
     }
+}
+
+int busca_linear (int *v, int tamanho, int x) {
+    for (int i = 0; i < tamanho; i++) {
+        if (v[i] == x) {
+            return 1;
+        }
+    }
+    return 0; 
+}
+
+void bubble (int *v, int tamanho) {
+    for (int i=1; i<tamanho; i++) {
+        for (int j=0; j<tamanho-i; j++) {
+            if(v[j] > v[j+1]) {
+                int temp = v[j];
+                v[j] = v[j+1];
+                v[j+1] = temp;
+            }
+        }
+    }   
 }
 
 int main () {
@@ -36,7 +57,19 @@ int main () {
     int *v = (int *) malloc (tamanho * sizeof(int));
     //le_vetor (v, tamanho);
     preenche_vetor (v, tamanho);
+    printf("vetor original: \n");
     mostra_vetor (v, tamanho);
-    
+    printf("\ndigite o valor para buscar: ");
+    int x;
+    scanf("%d", &x);
+    if (busca_linear (v, tamanho, x)) {
+        printf("%d foi encontrado no vetor pela busca linear\n", x);
+    } else {
+        printf("%d nao foi encontrado no vetor pela busca linear\n", x);
+    }
+
+    bubble (v, tamanho);
+    printf ("\nvetor ordenado pelo bubble:\n");
+    mostra_vetor (v, tamanho);
     return 0;
 }
